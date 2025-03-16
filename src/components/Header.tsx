7dd0953d869c53cbf41ec49e92f3fd3819d98711
aLi <alieshkevari1908@gmail.com>
2025-03-16 15:59:20 +0330

    header completed

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Header() {
   return (
     <div>
-      <h2 className="bg-amber-700"></h2>
+      <h2 className="bg-amber-700">IMDb Clone</h2>
     </div>
   );
 }
